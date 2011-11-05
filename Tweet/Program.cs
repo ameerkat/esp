d600edd7497154b6c56383a16d1db3fa83c76aa2
Travis Sein <tsein@asu.edu>
2011-11-04 22:18:22 -0700

    first commit

---
+++ Tweet/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Net;
+using System.Threading;
+
+namespace Tweet
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            String twitterStreamUrl = "https://stream.twitter.com/1/statuses/filter.json?track=fire";
+            String twitterUsername = "TeamTitansAZ";
+            String twitterPassword = "password";
+
+            TwitterEater twitterEater = new TwitterEater(twitterStreamUrl, twitterUsername, twitterPassword);
+            twitterEater.Start();
+
+
+            int numThreads = 4;
+            Thread[] threads = new Thread[numThreads];
+
+            for (int i = 0; i < numThreads; i++)
+            {
+                Thread thread = new Thread(new ParameterizedThreadStart(Consume));
+                object[] parameters = new object[] { twitterEater, i };
+                thread.Start(parameters);
+            }
+        }
+
+        private static void Consume(object parameters)
+        {
+            object[] parameterArray = (object[])parameters;
+            TwitterEater twitterEater = (TwitterEater)parameterArray[0];
+            int threadNumber = (int)parameterArray[1];
+            String parseMe;
+            while (true)
+            {
+                if ((parseMe = twitterEater.ReadLine()) != null)
+                {
+                    //Parse this
+                    Console.Out.WriteLine("Thread " + threadNumber + ": \t" + parseMe);
+                }
+                else
+                {
+                    Console.Out.WriteLine("Thread " + threadNumber + ": \tNothing to parse, waiting...");
+                    Thread.Sleep(1000);
+                }
+            }
+        }
+    }
+}
---
+++ Tweet/TwitterEater.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Net;
+using System.Threading;
+
+namespace Tweet
+{
+    class TwitterEater
+    {
+        private Queue<String> unparsedTweets;
+        private String twitterStreamUrl, twitterUsername, twitterPassword;
+        private Thread tweetReader;
+
+        public TwitterEater(String twitterStreamUrl, String twitterUsername, String twitterPassword)
+        {
+            this.twitterStreamUrl = twitterStreamUrl;
+            this.twitterUsername = twitterUsername;
+            this.twitterPassword = twitterPassword;
+            unparsedTweets = new Queue<String>();
+        }
+
+        private void DoRead()
+        {
+            WebRequest twitterRequest;
+            WebResponse twitterResponse;
+            StreamReader twitterStream;
+
+            twitterRequest = (HttpWebRequest)WebRequest.Create(twitterStreamUrl);
+            twitterRequest.Credentials = new NetworkCredential(twitterUsername, twitterPassword);
+            twitterRequest.Timeout = -1;
+            twitterResponse = (HttpWebResponse)twitterRequest.GetResponse();
+            Encoding encode = System.Text.Encoding.GetEncoding("utf-8");
+            twitterStream = new StreamReader(twitterResponse.GetResponseStream(), encode);
+
+            string line;
+            while ((line = twitterStream.ReadLine()) != null)
+            {
+                //Each line from the stream is an unparsed tweet
+                try
+                {
+                    unparsedTweets.Enqueue(line);
+                }
+                catch (Exception e)
+                {
+                    //Catch all for now
+                    throw new Exception("Reading failed at line: " + line);
+                }
+            }
+        }
+
+        public void Start()
+        {
+            try
+            {
+                tweetReader = new Thread(new ThreadStart(DoRead));
+                tweetReader.Start();
+            }
+            catch (Exception e)
+            {
+                //Catch all for now
+                Console.Out.WriteLine(e.Message);
+            }
+            
+        }
+
+        public void Stop()
+        {
+            tweetReader.Abort();
+        }
+
+        public String ReadLine()
+        {
+            lock(unparsedTweets)
+            {
+                String result = null;
+                try
+                {
+                    result = unparsedTweets.Dequeue();
+                }
+                catch (InvalidOperationException e)
+                {
+                    Console.Out.WriteLine("Empty queue");
+                }
+                return result;
+            }
+        }
+    }
+}
